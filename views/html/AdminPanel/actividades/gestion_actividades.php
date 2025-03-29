7d3fe7b4c9bf6c8215f516de79dd1b73eebeec36
juan-dgs <juan.d78@gmail.com>
2025-03-29 17:38:10 -0600

    Avance formulario nueva act

--- views/html/AdminPanel/actividades/gestion_actividades.php
+++ views/html/AdminPanel/actividades/gestion_actividades.php
@@ -1,11 +1,33 @@
 <?php
-include(HTML.'AdminPanel/masterPanel/head.php');
-include(HTML.'AdminPanel/masterPanel/navbar.php');
-include(HTML.'AdminPanel/masterPanel/menu.php');
-include(HTML.'AdminPanel/masterPanel/breadcrumb.php');
+    include(HTML.'AdminPanel/masterPanel/head.php');
+    include(HTML.'AdminPanel/masterPanel/navbar.php');
+    include(HTML.'AdminPanel/masterPanel/menu.php');
+    include(HTML.'AdminPanel/masterPanel/breadcrumb.php');
+
+
+    $qTipo = 'SELECT codigo,descripcion FROM act_c_tipos WHERE activo=1;';
+    $dt_tipo=findtablaq($qTipo,"codigo");
+
+    $qClasifica = 'SELECT codigo,descripcion FROM act_c_clasificacion WHERE activo = 1;';
+    $dt_clasifica=findtablaq($qClasifica,"codigo");
+
+    $qPrio = 'SELECT codigo,descripcion,hr_min,hr_max FROM act_c_prioridades WHERE activo = 1;';
+    $dt_prio=findtablaq($qPrio,"codigo");
+
+    $qCliente= 'SELECT id,concat(alias," - ",razon_social) as nombre FROM act_c_clientes WHERE activo = 1;';
+    $dt_cliente=findtablaq($qCliente,"id");
+
+
+    $qUsuarios= 'SELECT id,concat(nombre," ",apellido_p," ",apellido_m) as nombre,usuario,c_tipo_usuario as tipo,dir_foto as foto FROM users WHERE activo =1;';
+    $dt_usuarios=findtablaq($qUsuarios,"id");
 
 ?>
 
+
+
+  <link rel="stylesheet" href="<?php echo ADMINLTE; ?>plugins/select2/css/select2.min.css">
+  <link rel="stylesheet" href="<?php echo ADMINLTE; ?>plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+  <script src="<?php echo ADMINLTE; ?>plugins/select2/js/select2.full.min.js"></script>
 
 
 <style>
@@ -28,12 +50,19 @@ include(HTML.'AdminPanel/masterPanel/breadcrumb.php');
         font-weight: bold;
     }
 
+    .novisible{
+        display:none;
+    }
+
 </style>
+
+
 
 <button class="btn btn-primary" type="button" onclick="newActividad();" style="    position: absolute;    z-index: 1000;"><i class="fa fa-plus"></i>Nueva Actividad</button>
 
 <div id="contentActs">
 </div>
+      
 
 <div id="modalFormularioActiviades" class="modal fade" role="dialog">
     <div class="modal-dialog modal-lg">
@@ -47,66 +76,74 @@ include(HTML.'AdminPanel/masterPanel/breadcrumb.php');
                     <div class="col-xs-12 col-sm-6">
                         <div class="form-group">
                             <label for="c_tipo_actividad">Tipo de Actividad:</label>
-                            <select  id="c_tipo_actividad"></select>
+                            <select class="form-control select2ModalActividades" id="c_tipo_actividad">
+                            <?php     foreach ($dt_tipo as $codigo => $array) {
+                              echo "<option value='".$dt_tipo[$codigo]["codigo"]."'>".$dt_tipo[$codigo]["descripcion"]."</option>";
+                          } ?>
+                            </select>
                         </div>
                     </div>
 
                     <div class="col-xs-12 col-sm-6">
                         <div class="form-group">
                             <label for="c_clasifica_act">Clasificación:</label>
-                            <select  id="c_clasifica_act"></select>
+                            <select class="form-control select2ModalActividades" id="c_clasifica_act">
+                            <option value="">Seleciona Clasificación</option>
+                            <?php     foreach ($dt_clasifica as $codigo => $array) {
+                              echo "<option value='".$dt_clasifica[$codigo]["codigo"]."'>".$dt_clasifica[$codigo]["descripcion"]."</option>";
+                          } ?>
+                            </select>
                         </div>
                     </div>
 
                     <div class="col-xs-12 col-sm-6">
                         <div class="form-group">
                             <label for="c_tipo_actividad">Prioridad:</label>
-                            <select  id="c_prioridad"></select>
+                            <select class="form-control select2ModalActividades" id="c_prioridad">
+                            <option value="">Seleciona Prioridad</option>
+                            <?php     foreach ($dt_prio as $codigo => $array) {
+                              echo "<option value='".$dt_prio[$codigo]["codigo"]."' title='SLA max ".$dt_prio[$codigo]["hr_max"]." hrs - min ".$dt_prio[$codigo]["hr_min"]." hrs'>".$dt_prio[$codigo]["descripcion"]."</option>";
+                          } ?>
+                            </select>
                         </div>
                     </div>
 
                     <div class="col-xs-12 col-sm-6">
                         <div class="form-group">
-                            <label for="c_tipo_actividad">Cliente:</label>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text" id="id_cliente"></div>
-                                </div>
-                                <input type="text" class="form-control" placeholder="Buscar Cliente" id="searchCliente">
-                                <div class="input-group-prepend">
-                                    <button type="button" class="btn btn-default"><i class="fa fa-search"></i></button>
-                                </div>
-                            </div>
+                            <label for="c_cliente">Cliente:</label>
+                            <select class="form-control select2ModalActividades" id="c_cliente">
+                                <option value="">Seleciona Cliente Responsable</option>
+                                <?php     foreach ($dt_cliente as $codigo => $array) {
+                                echo "<option value='".$dt_cliente[$codigo]["id"]."'>".$dt_cliente[$codigo]["nombre"]."</option>";
+                            } ?>
+                            </select>
+                        </div>
+                    </div>
+
+
+                    <div class="col-xs-12 col-sm-6">
+                        <div class="form-group" style="position:relative;">
+                            <label for="c_usuario_responsable">Usuario Responsable:</label>
+                            <select class="form-control select2ModalActividades" id="c_usuario_responsable">
+                            <option value="">Seleciona Usuarios Responsable</option>
+                                <?php     foreach ($dt_usuarios as $codigo => $array) {
+                                echo "<option value='".$dt_usuarios[$codigo]["id"]."' foto='".($dt_usuarios[$codigo]["foto"]!=''?$dt_usuarios[$codigo]["foto"]:'userDefault.png')."' usuario='".$dt_usuarios[$codigo]["usuario"]."' tipo='".$dt_usuarios[$codigo]["tipo"]."' title='".$dt_usuarios[$codigo]["usuario"]." ".$dt_usuarios[$codigo]["tipo"]."'>".$dt_usuarios[$codigo]["nombre"]."</option>";
+                            } ?>
+                            </select>
+                            <div id="c_usuario_responsable_foto" class="circular novisible" style="background-image: url(views/images/profile/userDefault.png);background-size:  cover; width: 60px;height: 60px;border: solid 2px #fff;position: absolute;top: 0;right: 16px; "></div>
                         </div>
                     </div>
 
                     <div class="col-xs-12 col-sm-6">
                         <div class="form-group">
-                            <label for="searchUsuarioP">Usuario Responsable:</label>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <div title="Usuario Responsable: Luis Grardoa Saucedo" class="circular" style="background: url(views/images/profile/userDefault.png);background-size: cover;width: 60px;height: 60px;border: solid 2px #fff;margin-top: -13px; "></div>
-                                </div>
-                                <input type="text" class="form-control" placeholder="Buscar Usuario" id="searchUsuarioP">
-                                <div class="input-group-prepend">
-                                    <button type="button" class="btn btn-default"><i class="fa fa-search"></i></button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xs-12 col-sm-6">
-                        <div class="form-group">
-                            <label for="searchUsuarioI">Usuarios Involucrados:</label>
-                            <div class="input-group">                               
-                                <input type="text" class="form-control" placeholder="Buscar Usuario" id="searchUsuarioI">
-                                <div class="input-group-prepend">
-                                    <button type="button" class="btn btn-default"><i class="fa fa-search"></i></button>
-                                </div>
-                            </div>
-                            <div title="Usuario Responsable: Luis Grardoa Saucedo" class="circular" style="background: url(views/images/profile/userDefault.png);  background-size:  cover; width:40px; height: 40px;  border: solid 2px #fff; "></div>
-                            <div title="Usuario Responsable: Luis Grardoa Saucedo" class="circular" style="background: url(views/images/profile/userDefault.png);  background-size:  cover; width:40px; height: 40px;  border: solid 2px #fff; "></div>
-                            <div title="Usuario Responsable: Luis Grardoa Saucedo" class="circular" style="background: url(views/images/profile/userDefault.png);  background-size:  cover; width:40px; height: 40px;  border: solid 2px #fff; "></div>
+                            <label for="c_usuarios_ivolucrados">Usuarios Involucrados:</label>
+                            <select class="form-control select2ModalActividades" id="c_usuarios_ivolucrados" name="ivolucrados[]" multiple="multiple">>
+                                <?php     foreach ($dt_usuarios as $codigo => $array) {
+                                echo "<option value='".$dt_usuarios[$codigo]["id"]."' foto='".($dt_usuarios[$codigo]["foto"]!=''?$dt_usuarios[$codigo]["foto"]:'userDefault.png')."' usuario='".$dt_usuarios[$codigo]["usuario"]."' tipo='".$dt_usuarios[$codigo]["tipo"]."' title='".$dt_usuarios[$codigo]["usuario"]." ".$dt_usuarios[$codigo]["tipo"]."'>".$dt_usuarios[$codigo]["nombre"]."</option>";
+                            } ?>
+                            </select>
+                            <div id="c_usuario_involucrados_fotos"></div>
+                            
                         </div>
                     </div>
 
@@ -180,7 +217,51 @@ include(HTML.'AdminPanel/masterPanel/breadcrumb.php');
 $(document).ready(function(){
     getActividades();
 
+    $('.select2ModalActividades').select2({
+      theme: 'bootstrap4',
+      dropdownParent: $('#modalFormularioActiviades')
+    });
+    
+    
+    setTimeout(defineFnCUR(), 3000);
 });
+
+function defineFnCUR(){
+        $(".select2ModalActividades").change(function(){
+           if($(this).attr("id")=='c_usuario_responsable'){
+            $('#c_usuarios_ivolucrados option').prop("disabled",false);
+            
+            if($(this).val()==''){
+                $("#c_usuario_responsable_foto").addClass('novisible').css('background-image','');
+            }else{
+                $("#c_usuario_responsable_foto").removeClass('novisible').css('background-image','url("views/images/profile/'+$('option:selected',this).attr("foto")+'")');
+                $('#c_usuarios_ivolucrados option[value="' + $(this).val() + '"]').prop("disabled",true);
+
+                if($('#c_usuarios_ivolucrados').val().indexOf($(this).val())>=0){
+                    $('#c_usuarios_ivolucrados').select2().val($(this).val()).trigger("change");
+                }
+
+            }
+                fotosInvolucrados($('#c_usuarios_ivolucrados').val());
+
+           }else if($(this).attr("id")=='c_usuarios_ivolucrados'){
+                fotosInvolucrados($(this).val());
+           }
+        });
+    }
+
+    function fotosInvolucrados(arr){
+        var fotos ='';
+        if(arr.length>0){
+            $.each(arr, function( index, value ) {
+                fotos += '<div title="Usuario Involucrado: '+$('#c_usuarios_ivolucrados option[value="' + value + '"]').html() +'" class="circular" style="background-image: url(views/images/profile/'+$('#c_usuarios_ivolucrados option[value="' + value + '"]').attr("foto")+');  background-size:  cover; width:40px; height: 40px;  border: solid 2px #fff; "></div>';
+            });
+        }
+      
+
+        $("#c_usuario_involucrados_fotos").html(fotos);
+    }
+
 
     function getActividades(){
         $.ajax({
